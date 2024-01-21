80302ed893e9a1a8dcd19af220aa260726fd38d5
Eugenio <eugeniouglov@gmail.com>
2024-01-21 21:03:55 +0100

    Refactoring

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -32,7 +32,7 @@ public class Main : MonoBehaviour
         _actionBlockController.Init();
         
 
-        IndexingFilesFromFilesToIndexDirectory();
+        
         _loaderFullscreenService.Hide();
     }
 
@@ -44,73 +44,6 @@ public class Main : MonoBehaviour
     //     }
     // }
 
-    private void IndexingFilesFromFilesToIndexDirectory()
-    {
-        string indexedFilesDirectory = @"Admin\IndexedFiles\";
-        string filesToIndexDirectory = @"Admin\FilesToIndex\";
-
-        List<string> filePathsToIndex = new List<string>();
-
-        DirectoryManager directoryManager = new DirectoryManager();
-
-        DirectoryInfo indexedFilesDirectoryInfo = new DirectoryInfo(indexedFilesDirectory);
-        DirectoryInfo filesToIndexDirectoryInfo = new DirectoryInfo(filesToIndexDirectory);
-
-        directoryManager.CreateDirectoryIfDoesNotExist(indexedFilesDirectory);
-        directoryManager.CreateDirectoryIfDoesNotExist(filesToIndexDirectory);
-
-        MoveFilesToIndexedFilesFolder(filesToIndexDirectoryInfo, indexedFilesDirectoryInfo);
-        // RemoveFolders(filesToIndexDirectoryInfo);
-        directoryManager.RemoveEmptyFolders(filesToIndexDirectory);
-
-        _actionBlockController.CreateActionBlocksByPaths(paths: filePathsToIndex.ToArray(), isShowError: true);
-
-
-        // var allDirectories = Directory.GetDirectories(indexedFilesDirectory, "*", SearchOption.AllDirectories);
-        // foreach (string dir in allDirectories)
-        // {
-        //     string dirToCreate = dir.Replace(indexedFilesDirectory, filesToIndexDirectory);
-        //     Directory.CreateDirectory(dirToCreate);
-        // }
-
-        // Move files to index directory.
-        // StartCoroutine(GetFilesFromDirectoryAsync(
-        //     directory: filesToIndexDirectory, 
-        //     onGetFile: (file) => {
-        //         Directory.Move(Path.GetDirectoryName(file) + "\\" + Path.GetFileName(file), indexedFilesDirectory + Path.GetFileName(file));
-        //     })
-        // );
-
-
-        void MoveFilesToIndexedFilesFolder(DirectoryInfo source, DirectoryInfo target) 
-        {
-            Directory.CreateDirectory(target.FullName);
-
-            foreach (var file in source.GetFiles())
-            {
-                string targetPath = Path.Combine(target.FullName, file.Name);
-
-                if (File.Exists(targetPath))
-                {
-                    print("File already exists: " + targetPath);
-                    continue;
-                }
-                else 
-                {
-                    print("Move file " + targetPath);
-                    targetPath = targetPath.Substring(targetPath.IndexOf("Admin"));
-                    file.MoveTo(targetPath);
-                    filePathsToIndex.Add(targetPath);
-                }
-            }
-
-            foreach (var sourceSubdirectory in source.GetDirectories())
-            {
-                var targetSubdirectory = target.CreateSubdirectory(sourceSubdirectory.Name);
-                MoveFilesToIndexedFilesFolder(sourceSubdirectory, targetSubdirectory);
-            }
-        }
-    }
     
     public void Quit()
     {
